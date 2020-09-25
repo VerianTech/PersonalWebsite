bbd7d1be8a209b431342bb2babceb1e13a6e7830
dorianverna17 <dorianverna7@gmail.com>
2020-09-26 01:05:08 +0300

    Fourth commit

--- script_file.js
+++ script_file.js
@@ -2,11 +2,15 @@ function function1() {
     var x = document.getElementsByClassName("long_line");
     var y = document.getElementById("short_line");
     var z = document.getElementById("second_container");
+    var q = document.getElementsByClassName("menu_item");
     var i;
 
     z.style.transition = "all 2s";
     y.style.transition = "all 2s";
     for (i = 0; i < x.length; i++) {
         x[i].style.transition = "all 2s";
+    }
+    for (i = 0; i < q.length; i++) {
+        q[i].style.transition = "all 2s";
     }
 }
