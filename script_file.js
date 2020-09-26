f1e306e3a20cc16f216d6432d2789d17fb4abff2
dorianverna17 <dorianverna7@gmail.com>
2020-09-27 00:10:16 +0300

    Fifth commit

--- script_file.js
+++ script_file.js
@@ -1,4 +1,5 @@
 function function1() {
+    setTimeout(resizeIframe, 1500, document.getElementById("frame1"));
     var x = document.getElementsByClassName("long_line");
     var y = document.getElementById("short_line");
     var z = document.getElementById("second_container");
@@ -11,6 +12,13 @@ function function1() {
         x[i].style.transition = "all 2s";
     }
     for (i = 0; i < q.length; i++) {
-        q[i].style.transition = "all 2s";
+        q[i].style.transition = "opacity,font-size 2s";
     }
+}
+
+function resizeIframe(obj) {
+    obj.style.transition = "all 4s, opacity 0s";
+    obj.style.width = 75 + '%';
+    obj.style.height = 90 + '%';
+    obj.style.opacity = 1;
 }
