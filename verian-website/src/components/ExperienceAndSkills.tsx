90bbb2548595009b281574180a55a7d7972c320d
dorianverna17 <dorianverna7@gmail.com>
2025-05-20 22:50:43 +0200

    Added Experience section to website.

---
+++ verian-website/src/components/ExperienceAndSkills.tsx
@@ -0,0 +1,76 @@
+export default function ExperienceAndSkills() {
+  return (
+	<div className="grid grid-cols-2 justify-center text-center py-4 px-4 items-center
+		font-bold text-gray-300 text-sm gap-4 md:gap-6">
+	  <div>
+		<div className="grid grid-cols-1 justify-center gap-y-4 py-4 px-2 rounded-lg"
+			style={{
+				backgroundImage: "linear-gradient(#8aa0a7,#176b87)",
+			}}
+		>
+			<div className="grid grid-cols-10 place-items-center text-center">
+				<img
+					src="./src/assets/google.png"
+					alt="logo"
+					className="h-8 sm:h-10 md:h-12 lg:h-14 w-auto mx-autor col-span-3" />
+				<div className="flex items-center justify-center col-span-7">
+					Software Engineer @ Google<br/>
+					Sept. 2024 - Present
+				</div>
+			</div>
+			<div className="grid grid-cols-10 place-items-center text-center">
+				<img
+					src="./src/assets/hella.png"
+					alt="logo"
+					className="h-8 sm:h-10 md:h-12 lg:h-14 w-auto mx-autor col-span-3" />
+				<div className="flex items-center justify-center col-span-7">
+					Software Engineer @ Hella<br/>
+					Sept. 2024 - Present
+				</div>
+			</div>
+			<div className="grid grid-cols-10 place-items-center text-center">
+				<img
+					src="./src/assets/google.png"
+					alt="logo"
+					className="h-8 sm:h-10 md:h-12 lg:h-14 w-auto mx-autor col-span-3" />
+				<div className="flex items-center justify-center col-span-7">
+					Site Reliability Engineer Intern @ Google<br/>
+					Sept. 2024 - Present
+				</div>
+			</div>
+			<div className="grid grid-cols-10 place-items-center text-center">
+				<img
+					src="./src/assets/thales.png"
+					alt="logo"
+					className="h-8 sm:h-10 md:h-12 lg:h-14 w-auto mx-autor col-span-3" />
+				<div className="flex items-center justify-center col-span-7">
+					DevOps Engineer @ Thales<br/>
+					Sept. 2024 - Present
+				</div>
+			</div>
+			<div className="grid grid-cols-10 place-items-center text-center">
+				<img
+					src="./src/assets/google.png"
+					alt="logo"
+					className="h-8 sm:h-10 md:h-12 lg:h-14 w-auto mx-autor col-span-3" />
+				<div className="flex items-center justify-center col-span-7">
+					Software Engineer Intern @ Google<br/>
+					Sept. 2024 - Present
+				</div>
+			</div>
+			<div className="grid grid-cols-10 place-items-center text-center">
+				<img
+					src="./src/assets/nxp.png"
+					alt="logo"
+					className="h-8 sm:h-10 md:h-12 lg:h-14 w-auto mx-autor col-span-3" />
+				<div className="flex items-center justify-center col-span-7">
+					Test Developer Intern @ NXP<br/>
+					Sept. 2024 - Present
+				</div>
+			</div>
+		</div>
+	  </div>
+	  <div>Skills</div>
+	</div>
+  );
+}
