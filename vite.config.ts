88dbd4d63a44564a4203c41abef42fcccfb854d5
dorianverna17 <dorianverna7@gmail.com>
2025-05-23 22:58:59 +0200

    Modified plugin in vite config as to include all assets.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/PersonalWebsite/",
-  plugins: [react()],
+  plugins: [react(),
+	viteStaticCopy({
+		targets: [
+		  {
+			src: 'src/assets/*',
+			dest: 'src/assets/',
+		  }
+		]
+	  })
+  ],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
+  assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg', '**/*.webp']
 })
